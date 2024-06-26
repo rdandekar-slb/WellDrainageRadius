ccbaec8a1b75ffdae29ec50ebd3337788252c68d
Rashmin Dandekar <rdandekar@slb.com>
2024-06-27 07:58:20 +0800

    Few more features added and removed

--- firstshapely.py
+++ firstshapely.py
@@ -40,18 +40,21 @@ myfield:fm.Field = fm.Field("Brillig")
 
 myfield.map=sh.Polygon((sh.Point(0,0),sh.Point(0,50),sh.Point(50,50),sh.Point(50,0),sh.Point(0,0)))
 myfield.get_map_from_cps3(r"C:\Users\rdandekar\Desktop\DLs\Data\Brillig_8080")
-myfield.get_faults_from_cps3(r"C:\Users\rdandekar\Desktop\DLs\Data\Fault10.cps",r"C:\Users\rdandekar\Desktop\DLs\Data\Fault20.cps")
+# myfield.get_faults_from_cps3(r"C:\Users\rdandekar\Desktop\DLs\Data\Fault10.cps",r"C:\Users\rdandekar\Desktop\DLs\Data\Fault20.cps")
+
+# mygeom=sh.union(myfield.map,myfield.faults)
+# print(sh.get_type_id(mygeom))
 map_bounds=get_map_bounds(myfield.map)
-print(map_bounds)
+# print(map_bounds)
 fig,ax=plt.subplots()
 fig.set_dpi(90)
 fig.set_figheight(8)
 fig.set_figwidth(8)
 ax.set_title(myfield.name)
 shplt.plot_polygon(myfield.map,ax=ax,add_points=False)
-shplt.plot_line(myfield.faults,ax=ax,add_points=False)
+# shplt.plot_line(myfield.faults,ax=ax,add_points=False)
 set_limits(ax,map_bounds[0]-1,map_bounds[2]+1,map_bounds[1]-1,map_bounds[3]+1)
-plt.show()
+# plt.show()
 
 
 
